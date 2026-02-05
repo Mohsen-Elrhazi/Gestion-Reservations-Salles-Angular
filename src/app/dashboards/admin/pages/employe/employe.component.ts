62340c6111689821a16a97905c141662e3c7ded9
Mohsen-web-coding <mohsenelrhazi3@gmail.com>
2026-02-05 16:49:34 +0100

    Créer la methode creerEmploye dans employe.componenet et utilise la methode creerEmploye de service

--- src/app/dashboards/admin/pages/employe/employe.component.ts
+++ src/app/dashboards/admin/pages/employe/employe.component.ts
@@ -1,20 +1,23 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Employe } from '../../models/employe/employe.model';
 import { EmployeService } from '../../services/employe/employe.service';
-import {CommonModule} from "@angular/common";
+import { CommonModule } from '@angular/common';
+import { CreateEmployeRequest } from '../../models/employe/create-employe-request.model';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-employe',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './employe.component.html',
   styleUrl: './employe.component.css',
 })
 export class EmployeComponent implements OnInit {
   employes: Employe[] = [];
+  newEmploye: CreateEmployeRequest = { nom: '', email: '', motDePasse: '' };
 
   constructor(
     private employeService: EmployeService,
-    private cdr: ChangeDetectorRef
+    private cdr: ChangeDetectorRef,
   ) {}
 
   ngOnInit(): void {
@@ -29,7 +32,19 @@ export class EmployeComponent implements OnInit {
       },
       error: (err) => {
         console.error('Error fetching employes:', err);
-      }
+      },
+    });
+  }
+
+  createEmploye() {
+    this.employeService.createEmploye(this.newEmploye).subscribe({
+      next: (response) => {
+        console.log('employé crée: ', response.data);
+        this.newEmploye={nom: '', email: '', motDePasse: ''};
+        this.loadEmployes();
+         const closeBtn= document.getElementById('btnCloseModal');
+        closeBtn?.click();
+      },
     });
   }
 }
